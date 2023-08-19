e400cdf950478b04f5fa3a2b6187d830e7c2ba99
fpS-AUDIO <saudio.ru@gmail.com>
2023-08-19 17:38:39 +0200

    Finished styling the scrollbar. Need to fix the header typewrite animation.

--- static/js/main_script.js
+++ static/js/main_script.js
@@ -1,35 +1,23 @@
 
 
-////////////////////
-// theme switcher //
-////////////////////
+////////////////////////
+// TYPEWRITING HEADER //
+////////////////////////
 
-/* let themeSwithcer = document.getElementById("themeSwitch"); 
-let logoImage = document.getElementById("logo-img");
 
-themeSwithcer.onclick = function(){
-    document.body.classList.toggle("light-theme");
-    if(document.body.classList.contains("light-theme")){
-        // logoImage.src = "static/img/logo-ivanov-white.png";
-        logoImage.src="{% static 'img/logo-ivanov-white.png' %}";
-    }else{
-        // logoImage.src = "static/img/logo-ivanov-black.png";
-        logoImage.src="{% static 'img/logo-ivanov-black.png' %}";
-    }
-} */
 
-/* 
+const headerText = document.querySelector("#header-text");
+const textLoad = () => {
+    setTimeout(() => {
+        headerText.textContent = "Web Developer";
+    }, 0);
+    setTimeout(() => {
+        headerText.textContent = "Problem solver";
+    }, 4000);
+    setTimeout(() => {
+        headerText.textContent = "Teaholic";
+    }, 8000);
+}
 
-The issue you're facing is due to the way Django's template 
-engine works. The template engine processes the Django template 
-tags ({% static %} in your case) on the server-side before 
-sending the HTML response to the client. This means that your 
-JavaScript code, which runs on the client-side, doesn't have 
-access to Django's template tags.
-
-To solve this problem, you need to pass the paths of the 
-images to your JavaScript code from the server-side. 
-One way to do this is by using Django ...
-
-*/
-
+textLoad();
+setInterval(textLoad, 12000);
