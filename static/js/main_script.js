8a86d7256877b29362751aa0a32bfa73eda8ea8b
fpS-AUDIO <saudio.ru@gmail.com>
2023-08-21 23:20:58 +0200

    Implemented changing hand-writing text in the head. Need to adjust timing anche colors.

--- static/js/main_script.js
+++ static/js/main_script.js
@@ -4,20 +4,29 @@
 // TYPEWRITING HEADER //
 ////////////////////////
 
+const text = document.querySelector(".jumbo-span");
+const texts = [
+    "Web Developer",
+    "Problem Solver",
+    "Teaholic"
+];
 
+let currentTextIndex = 0;
 
-const headerText = document.querySelector("#header-text");
+const updateAnimation = () => {
+    const contentWidth = text.clientWidth * 2;
+    const steps = Math.round(contentWidth);
+    const animationValue = "flashing-border 800ms step-start infinite both, moving-span-after 5000ms steps(${steps}) infinite";
+    text.style.animation = animationValue;
+};
+
 const textLoad = () => {
-    setTimeout(() => {
-        headerText.textContent = "Web Developer";
-    }, 0);
-    setTimeout(() => {
-        headerText.textContent = "Problem solver";
-    }, 4000);
-    setTimeout(() => {
-        headerText.textContent = "Teaholic";
-    }, 8000);
-}
+    text.textContent = texts[currentTextIndex];
+    updateAnimation();
+
+    currentTextIndex = (currentTextIndex + 1) % texts.length;
+};
 
 textLoad();
-setInterval(textLoad, 12000);
+setInterval(textLoad, 5000);
+
