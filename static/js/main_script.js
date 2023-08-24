496318f2e2305c472d3a23436ada1eabbaa77fc6
fpS-AUDIO <saudio.ru@gmail.com>
2023-08-24 17:40:45 +0200

    Added favicon. Changed bg-img in <header>.

--- static/js/main_script.js
+++ static/js/main_script.js
@@ -30,3 +30,64 @@ const textLoad = () => {
 textLoad();
 setInterval(textLoad, 5000);
 
+
+
+
+
+////////////////////////
+//// Theme Switcher ////
+////////////////////////
+
+let themeSwitcher = document.getElementById("themeSwitch");
+let logoImage = document.getElementById("logo-img");
+let HeaderBgImg = document.getElementById("HeaderId");
+
+themeSwitcher.onclick = function () {
+    document.body.classList.toggle("light-theme");
+    /*     if (document.body.classList.contains("light-theme")) {
+            HeaderBgImg.style.backgroundImage = `url('${HeaderBgImg.getAttribute('data-light-bg')}')`;
+        } else {
+            HeaderBgImg.style.backgroundImage = `url('${HeaderBgImg.getAttribute('data-dark-bg')}')`;
+        } */
+};
+
+
+
+
+
+//////////////////////////////////////
+//// Header Animations Bug Fixing ////
+//////////////////////////////////////
+
+// Add a delay before applying the animation on the delayed element
+// ELEMENT: <h2 class="animated-title anime-delay">a Full-Stack Web Developer</h2>
+setTimeout(function () {
+    document.querySelector('.anime-delay').style.borderRight = '4px solid var(--clr-flashing-border)';
+}, 4000); // Adjust the delay time as needed
+
+
+
+
+
+//////////////////////////////////////
+//// ............................ ////
+//////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
